4665fccb2812d40af90ea2645ab7a3bc6128f651
arpitnath <arpitnath42@gmail.com>
2021-04-19 21:46:49 +0530

    navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import commonStyles from './styles/common.module.css'
+import Logo from './assets/Logo.svg'
+import Navbar from './components/Navbar'
 import HomeScreen from './screens/HomeScreen'
 import LaunchScreen from './screens/LaunchScreen'
 
 function App() {
   return (
     <Router>
-      <div className={commonStyles.main}>
+      <div className='main'>
+        <Navbar logo={Logo} />
         <Switch>
           <Route path='/launch' component={LaunchScreen} />
           <Route path='/' component={HomeScreen} exact />
