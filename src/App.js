7ba56879c4c1b6928e82c3742d39f1e5e1b2c19d
arpitnath <arpitnath42@gmail.com>
2021-04-19 20:48:44 +0530

    LaunchScreen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import commonStyles from './styles/common.module.css'
 import HomeScreen from './screens/HomeScreen'
+import LaunchScreen from './screens/LaunchScreen'
 
 function App() {
   return (
     <Router>
       <div className={commonStyles.main}>
         <Switch>
+          <Route path='/launch' component={LaunchScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Switch>
       </div>
