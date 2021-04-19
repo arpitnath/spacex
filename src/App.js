805a9d836c925b27293dc4120c4f6ce7d22981c9
arpitnath <arpitnath42@gmail.com>
2021-04-19 20:44:05 +0530

    project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import commonStyles from './styles/common.module.css'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
-  return <div className='App'>test</div>
+  return (
+    <Router>
+      <div className={commonStyles.main}>
+        <Switch>
+          <Route path='/' component={HomeScreen} exact />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
