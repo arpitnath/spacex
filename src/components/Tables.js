c959f05a3f2c6caa1c279fd07dbeadfa310c7719
arpitnath <arpitnath42@gmail.com>
2021-04-20 05:00:15 +0530

    table data

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -2,36 +2,80 @@ import React from 'react'
 import screenStyles from '../styles/screens.module.css'
 
 const Tables = () => {
+  const launchData = [
+    {
+      id: 0,
+      launch: '22nd March 2020 at 2.30pm',
+      Location: 'California, United States',
+      Misson: 'Apolo II',
+      Orbit: 'Mars',
+      launch_status: 'success',
+      rokcet: 'Falcon 4'
+    },
+    {
+      id: 1,
+      launch: '22nd March 2020 at 2.30pm',
+      Location: 'California, United States',
+      Misson: 'Apolo II',
+      Orbit: 'Mars',
+      launch_status: 'success',
+      rokcet: 'Pikachuu'
+    },
+    {
+      id: 2,
+      launch: '22nd March 2020 at 2.30pm',
+      Location: 'California, United States',
+      Misson: 'Apolo II',
+      Orbit: 'Mars',
+      launch_status: 'success',
+      rokcet: 'Goku'
+    },
+    {
+      id: 3,
+      launch: '22nd March 2020 at 2.30pm',
+      Location: 'California, United States',
+      Misson: 'Apolo II',
+      Orbit: 'Mars',
+      launch_status: 'success',
+      rokcet: 'Falcon 22'
+    },
+    {
+      id: 4,
+      launch: '2nd October 2021 at 2.30pm',
+      Location: 'Mumbai, India',
+      Misson: 'Piku chanachuur',
+      Orbit: 'Galaxy',
+      launch_status: 'upcoming',
+      rokcet: 'Dora'
+    }
+  ]
+
   return (
     <div className='Wrapper'>
       <table className={screenStyles.Table}>
         <thead>
           <tr>
-            <th></th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>No:</th>
+            <th>Launch (UTC)</th>
+            <th>Location</th>
+            <th>Mission</th>
+            <th>Orbit</th>
+            <th>Launch Status</th>
+            <th>Rocket</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Larry</td>
-            <td>Page</td>
-            <td>@twitter</td>
-          </tr>
+          {launchData.map((launch, index) => (
+            <tr key={launch.id} onClick={() => console.log(launch.rokcet)}>
+              <td>{index + 1}</td>
+              <td>{launch.launch}</td>
+              <td>{launch.Location}</td>
+              <td>{launch.Misson}</td>
+              <td>{launch.Orbit}</td>
+              <td>{launch.launch_status}</td>
+              <td>{launch.rokcet}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
