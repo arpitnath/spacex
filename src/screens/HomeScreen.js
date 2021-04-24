0b3066b764cb28d1b33ba45f457abeed6dc7480e
arpitnath <arpitnath42@gmail.com>
2021-04-24 20:38:28 +0530

    homescreen stylings & counters

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,41 +1,50 @@
 import React from 'react'
 import Counter from '../components/Counters'
+import { useFetch } from '../Hooks'
 import styles from '../styles/screens.module.css'
 
 const HomeScreen = () => {
+  const { data, loading } = useFetch('https://api.spacexdata.com/v3/info')
+
   return (
-    <div className='Wrapper'>
-      <h1>HomeScreen</h1>
-      <div className={styles.Home}>
-        <div className={styles.Counters}>
-          <Counter
-            link='/users'
-            icon='😅'
-            title='title'
-            number={parseInt('448')}
-          />
-          <Counter
-            link='/institutions/companies'
-            icon='💙'
-            title='title'
-            number={parseInt('556')}
-          />
-          <Counter
-            link='/institutions/startups'
-            icon='🔐'
-            title='title'
-            number={parseInt('112')}
-          />
-          <Counter
-            link='/requests'
-            icon='🛠'
-            title='title'
-            number={parseInt('22')}
-          />
-        </div>
-      </div>
-      <div>something</div>
-    </div>
+    <>
+      {!loading ? (
+        'loading...!'
+      ) : (
+        <>
+          <div className={styles.Home}>
+            <h1>{data.summary.split('The')[0]}</h1>
+            <div className={styles.Counters}>
+              <Counter
+                link='/users'
+                icon='😅'
+                title='title'
+                number={parseInt('448')}
+              />
+              <Counter
+                link='/institutions/companies'
+                icon='💙'
+                title='title'
+                number={parseInt('556')}
+              />
+              <Counter
+                link='/institutions/startups'
+                icon='🔐'
+                title='title'
+                number={parseInt('112')}
+              />
+              <Counter
+                link='/requests'
+                icon='🛠'
+                title='title'
+                number={parseInt('22')}
+              />
+            </div>
+          </div>
+          <div>something</div>
+        </>
+      )}
+    </>
   )
 }
 
