fbdfa790f8da510c1936e03eacbcbf353f8b6e69
arpitnath <arpitnath42@gmail.com>
2021-05-29 14:36:32 +0530

    error boundary and history

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+import History from './utils/History'
+
+//pages OR components
+import ErrorBoundary from './components/ErrorBoundary'
+import Home from './pages/Home'
+
+const TestErr = () => {
+  return (
+    <>
+      <h2>Error Page</h2>
+    </>
+  )
+}
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <ErrorBoundary>
+      <Router history={History}>
+        <Switch>
+          <Route path={'/'} component={Home} exact />
+          <Route component={TestErr} />
+        </Switch>
+      </Router>
+    </ErrorBoundary>
   )
 }
 
---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error: React.FC = () => {
+  return (
+    <div>
+      <h2>Something went wrong</h2>
+      <a href='/'> ← Home</a>
+    </div>
+  )
+}
+
+export default Error
