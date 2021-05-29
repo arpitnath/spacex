858ed051f6c0c27cf546ca9938aaf64e563d2d84
arpitnath <arpitnath42@gmail.com>
2021-05-29 15:42:27 +0530

    launch page structure

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import styles from '../styles/scss/pages/launch.module.scss'
 
 const Launch: React.FC = () => {
   return (
-    <div style={{ marginTop: '100px' }}>
-      <h1>Launch Page</h1>
-    </div>
+    <>
+      {/* Loading state => `spinner` */}
+      <div className={styles.LaunchWrapper}>
+        {/* Filters */}
+        {/* Table */}
+        {/* Pagination */}
+        <h1>content</h1>
+      </div>
+    </>
   )
 }
 
