babf327bee02585b86753a2319c1c74ebc388975
arpitnath <arpitnath42@gmail.com>
2021-06-01 17:30:05 +0530

    modal structure

---
+++ src/components/Modal.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+//import icons and stylesheet
+
+const Modal: React.FC = () => {
+  //modal state
+
+  //close modal function
+
+  //useffect hook to set styles as there will be two modals one for datepicker and info
+  return (
+    <div>
+      <div>
+        {/* icon */}
+        {/* children */}
+      </div>
+    </div>
+  )
+}
+
+export default Modal
